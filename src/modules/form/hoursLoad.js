c26f9737f321d83b4315a70a46cae58bd932ec07
nicholaslouback <nilouback@hotmail.com>
2025-06-27 14:42:38 -0300

    feat: adding unavailable hours

--- src/modules/form/hoursLoad.js
+++ src/modules/form/hoursLoad.js
@@ -4,18 +4,23 @@ import { hoursClick } from "./hoursClick"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
 
   hours.innerHTML = ""
+
+  const unavailableHours = dailySchedules.map((schedule) => 
+    dayjs(schedule.when).format("HH-mm"))
 
   const opening = openingHours.map((hour) =>{
     const [sheduleHour] = hour.split(":")
 
-    const isHourPast = dayjs(date).add(sheduleHour, "Hour").isAfter(dayjs())
+    const isHourPast = dayjs(date).add(sheduleHour, "Hour").isBefore(dayjs())
+
+    const available = ! unavailableHours.includes(hour) && !isHourPast
 
     return {
       hour,
-      available: isHourPast,
+      available,
     }
 
   })
--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -12,5 +12,5 @@ export async function schedulesDay() {
 
   schedulesShow({ dailySchedules })
 
-  hoursLoad({ date })
+  hoursLoad({ date, dailySchedules })
 }
